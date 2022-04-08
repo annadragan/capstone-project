64f951132dc128ca554766c6f12b034c4265ec04
Anna Dragan <anulkadragan@gmail.com>
2022-04-08 15:05:39 +0200

    Add MathRandom quotes generator

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
-export default function Button({ category }) {
+export default function Button({ category, ...props }) {
   return (
-    <ButtonStyled aria-label="button" role="button">
+    <ButtonStyled {...props} aria-label="button" role="button">
       {category}
     </ButtonStyled>
   );
--- src/pages/IdiomaticExpressions.js
+++ src/pages/IdiomaticExpressions.js
@@ -1,5 +1,56 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import Button from '../components/Button';
 import Header from '../components/Header';
+import { idioms } from '../data';
 
 export default function IdiomaticExpressions() {
-  return <Header>Idiomatic Expressions</Header>;
+  const [quote, setQuote] = useState('');
+
+  return (
+    <>
+      <Header>Idiomatic Expressions</Header>
+      {/* {quote && ( */}
+      <>
+        {idioms.map(({ _id, expression, meaning }) => {
+          return (
+            <ExpressionWrapper
+              key={_id}
+              expression={expression}
+              meaning={meaning}
+            >
+              <ExpressionContent>
+                {idioms[quote] && idioms[quote].expression}
+              </ExpressionContent>
+              <ExpressionMeaning>
+                {idioms[quote] && idioms[quote].meaning}
+              </ExpressionMeaning>
+            </ExpressionWrapper>
+          );
+        })}
+      </>
+      {/* )} */}
+      <Button
+        category="next expression"
+        onClick={() => handleRandomeQuote()}
+      ></Button>
+    </>
+  );
+
+  function handleRandomeQuote() {
+    const quote = Math.floor(Math.random() * idioms.length);
+    setQuote(quote);
+  }
 }
+
+const ExpressionWrapper = styled.div`
+  padding: 20px;
+`;
+
+const ExpressionContent = styled.p`
+  font-size: 20px;
+  display: flex;
+  justify-content: center;
+`;
+
+const ExpressionMeaning = styled.p``;
