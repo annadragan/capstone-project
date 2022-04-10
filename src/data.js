7a1edd6a60076895a717dc8523c8cc21fb84420f
Anna Dragan <anulkadragan@gmail.com>
2022-04-10 19:24:33 +0200

    Add more idiomatic expressions

--- src/data.js
+++ src/data.js
@@ -299,6 +299,509 @@ export const idioms = [
     expression: '"Durch die Lappen gehen"',
     meaning: 'entwischen, verschwinden.',
   },
+  {
+    _id: nanoid(),
+    expression: '"Für jemanden eine Lanze brechen"',
+    meaning:
+      'ihn verteidigen, ihm beistehen. Aus der Rittersprache, wo sich bei Zweikämpfen der Sekundant im Falle einer bedrohlichen Situation schon mal einmischte und dabei den Bruch seiner Lanze, wenn nicht mehr, riskierte.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"ch bin mit meinem Latein am Ende"',
+    meaning: 'hier weiß ich nicht mehr weiter, das kenne ich zu wenig.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemandem den Laufpass geben"',
+    meaning: 'ihn entlassen oder mit ihm brechen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"(Frisch) vom Leder ziehen"',
+    meaning: 'sich rücksichtslos äußern, kein Blatt vor den Mund nehmen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die alte Leier "',
+    meaning:
+      ' immer dasselbe Thema. Mit der Drehleier, einem Musikinstrument aus dem Mittelalter, spielte man immer dieselbe Musik ab.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemandem auf den Leim gehen"',
+    meaning:
+      'von ihm hereingelegt/betrogen werden. Vogelfänger arbeiteten mit Leimruten, an denen die Vögel hängen blieben.',
+  },
+  {
+    _id: nanoid(),
+    expression:
+      '"Aus dem letzten Loch pfeifen (auch: auf dem letzten Loch pfeifen)"',
+    meaning: 'am Ende sein, nicht mehr können.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Den Löffel abgeben"',
+    meaning: 'sterben.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemanden über den Löffel barbieren"',
+    meaning: 'ihn betrügen oder benachteiligen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Im Lot sein"',
+    meaning:
+      'richtig bzw. in Ordnung sein. Aus der Maurersprache, wo das Bleilot anzeigt, ob die Mauer wirklich senkrecht steht.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemanden an die (frische) Luft setzen"',
+    meaning: 'ihn entlassen oder des Hauses verweisen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Lügen wie gedruckt "',
+    meaning:
+      '(Zeitungen sind gedruckt, Märchen, Romane sind erfundene Geschichten)    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Lunte riechen"',
+    meaning:
+      'Verdacht schöpfen. Die Lunte, also die Zündschnur, konnte man häufig weithin riechen und sich rechtzeitig vor dem Schuss in Sicherheit bringen',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Makulatur reden"',
+    meaning:
+      'Unsinn von sich geben. Makulatur ist unbrauchbares Druckpapier, das man nur noch für minderwertige Zwecke benutzen kann.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Den Mantel des Schweigens über etwas legen"',
+    meaning:
+      'nichts verraten, etwas verheimlichen. Sinnbildlich etwas unsichtbar machen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Er steht bei mir auf der Matte"',
+    meaning: 'er will etwas von mir. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Wieder auf der Matte stehen"',
+    meaning:
+      'wieder gesund sein, (nach längerer Abwesenheit) wieder Dienst tun.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Da beißt die Maus keinen Faden ab"',
+    meaning: 'das ist nicht mehr zu ändern.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Weiße Mäuse sehen"',
+    meaning: 'Wahnvorstellungen haben, vor allem im Rausch.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Es ist zum Mäusemelken"',
+    meaning:
+      'es ist absurd, wahnwitzig, unglaublich, auch: zum Verzweifeln    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Da geht mir das Messer in der Hose auf"',
+    meaning: 'meine Empörung ist nicht mehr zu überbieten.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ins offene Messer laufen "',
+    meaning: 'durch Leichtsinn oder Unaufmerksamkeit ins Verderben geraten.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Einen Metzgersgang machen"',
+    meaning:
+      'twas erfolglos unternehmen. Fleischer gingen früher von Hof zu Hof auf der Suche nach Arbeit. Kehrten sie am Abend ohne Auftrag zurück, hatten sie einen „Metzgersgang“ gemacht.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Das ist nicht auf meinem Mist gewachsen"',
+    meaning: 'stammt nicht von mir, ich habe damit nichts zu tun.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Hinter dem Mond leben "',
+    meaning:
+      'wirklichkeitsfremd sein. Vom irdischen Betrachter aus befindet sich jemand, der hinter dem Mond lebt, auf dessen Rückseite. Wer aber dort lebt, der hat den Mond zwischen sich und der Erde und somit keinen Blick auf das, was dort geschieht.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemanden zum Mond schießen"',
+    meaning: 'sich unsanft von jemandem trennen',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Er macht gerne aus einer Mücke einen Elefanten"',
+    meaning:
+      'eine unbedeutende Sache ungemein aufbauschen / maßlos übertreiben.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Etwas für bare Münze nehmen "',
+    meaning: 'etwas ernst nehmen, obwohl nur im Scherz gesprochen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Den Mund halten"',
+    meaning: 'still sein, schweigen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Sich den Mund verbrennen"',
+    meaning:
+      'das Falsche sagen, unbequeme bzw. peinliche Dinge offen aussprechen    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Nicht auf den Mund gefallen sein "',
+    meaning: 'redegewandt oder schlagfertig sein.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"…, dann gute Nacht!"',
+    meaning: 'das wäre schlimm.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Eine Nachteule"',
+    meaning:
+      'jemand, der besonders nachts aktiv ist und spät heimkommt. Eulen sind Nachtvögel und lassen sich bei Tag kaum blicken.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"In einer Nacht-und-Nebel-Aktion"',
+    meaning:
+      'heimlich, vor der Öffentlichkeit verborgen. Im Dezember 1941 ließ die Wehrmacht Widerstandskämpfer und Nazigegner aus den besetzten Gebieten in Deutschland spurlos verschwinden.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Nadel im Heuhaufen suchen"',
+    meaning:
+      'etwas Unmögliches versuchen müssen. Es ist praktisch ausgeschlossen, dass die Suche erfolgreich sein wird.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Den Nagel auf den Kopf treffen"',
+    meaning:
+      'genau das Richtige sagen oder erraten. Aus der Schützensprache, wo mit Nagel der Mittelpunkt der zu treffenden Scheibe gemeint war. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Etwas an den Nagel hängen (wie den Beruf) "',
+    meaning: 'mit etwas aufhören. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Etwas brennt mir unter (seltener: auf) den Nägeln"',
+    meaning:
+      'Umschreibung einer inneren Situation der Ungeduld, wenn man eine drängende Angelegenheit hinter sich bringen oder eine Frage loswerden will.     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Nagelprobe bestehen"',
+    meaning: 'eine entscheidende Prüfung bestehen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Aus dem Nähkästchen plaudern"',
+    meaning: 'etwas sagen, was nicht für die Öffentlichkeit bestimmt ist.     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemandem auf der Nase herumtanzen "',
+    meaning: 'unter den Augen des Betreffenden tun, was man will.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ach du grüne Neune! "',
+    meaning: 'Ausdruck einer meist unangenehmen Überraschung. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Das ist ein Notnagel"',
+    meaning: 'eine Notlösung. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Eine Nullnummer "',
+    meaning:
+      'ein Vorhaben, das entgegen den Erwartungen erfolglos verlaufen ist, oder – im Fußball: ein abfälliger Ausdruck für ein Spiel, das 0:0 endete.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Den Nullpunkt erreichen "',
+    meaning: 'damit wird ausgesagt, etwas habe einen Tiefstpunkt erreicht.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Oberwasser haben bzw. bekommen"',
+    meaning: 'Im Vorteil sein bzw. die Oberhand gewinnen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Seinen Obolus entrichten"',
+    meaning: 'einen kleinen Beitrag zahlen ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Sich zum Obst (der Woche) machen"',
+    meaning: 'auffallen, um Aufmerksamkeit zu erregen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Nicht ohne sein "',
+    meaning:
+      '(bei Menschen) auffällige positive Eigenschaften haben, z. B. intelligent und pfiffig sein, oder auch negative Eigenschaften haben, z. B. unberechenbar sein. Beim Bezug auf Situationen oder anderes ebenfalls im Sinne von bemerkenswerten Umständen oder Eigenschaften.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Sich etwas hinter die Ohren schreiben"',
+    meaning:
+      'sich etwas gut merken. Zu wichtigen Ereignissen zogen unsere Vorfahren ihre Kinder als Zeugen bei und gaben ihnen einen Klaps hinter die Ohren, damit sie sich an den Vorgang besser erinnern und das Wissen an spätere Generationen weitergeben könnten.     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Es faustdick hinter den Ohren haben"',
+    meaning: 'durchtrieben oder raffiniert sein. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Noch grün hinter den Ohren sein"',
+    meaning: 'noch jung, unerfahren sein.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Sich aufs Ohr hauen"',
+    meaning: 'sich zum Schlafen oder zum Ausruhen hinlegen',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemanden übers Ohr hauen "',
+    meaning: 'jemanden auf unfaire Art und Weise betrügen oder austricksen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Öl ins Feuer gießen"',
+    meaning: 'anstacheln, ein Übel noch vermehren.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Frech wie Oskar"',
+    meaning: 'dreist, wagemutig, furchtlos oder unverschämt sein. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"die Ohren steif halten"',
+    meaning:
+      'tapfer bleiben, standhaft bleiben, sich nicht unterkriegen lassen    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Das sind zwei Paar Stiefel "',
+    meaning:
+      'das verhält sich ganz anders, das sind zwei unterschiedliche Dinge.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Das bringt mich auf die Palme"',
+    meaning: 'da werde ich wütend, da gehe ich in die Luft. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Stur wie ein Panzer sein"',
+    meaning:
+      'uneinsichtig sein, sich auch durch gute Argumente nicht von seiner Überzeugung abbringen lassen. Der Panzer machte den Ritter unverwundbar, aber auch unbeweglich.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ich kenne meine Pappenheimer"',
+    meaning:
+      'ich kenne diese Leute (und ihre Schwächen), ich weiß sie einzuschätzen.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemandem in die Parade fahren"',
+    meaning: 'seine Pläne durchkreuzen oder abblocken.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ein Paragraphenreiter "',
+    meaning:
+      'jemand, der sich hinter dem Wortlaut von Vorschriften verschanzt, der nicht zu einer liberalen oder vernünftigen Auslegung bereit ist.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemandem Paroli bieten"',
+    meaning: 'ihm zeigen, dass man ebenbürtig ist/gegenhält',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Pi mal Daumen"',
+    meaning: 'ungefähr. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemanden in die Pfanne hauen"',
+    meaning: 'jemanden verraten, etwas über ihn ausplaudern.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Du kannst da hin gehen, wo der Pfeffer wächst"',
+    meaning: 'geh weg und komm nicht wieder!',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Das Pferd von hinten aufzäumen"',
+    meaning:
+      'in der verkehrten Reihenfolge arbeiten (und damit scheitern).     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ihm sind die Pferde durchgegangen"',
+    meaning: 'er hat unbeherrscht gehandelt, die Nerven verloren.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Sache hat einen Pferdefuß"',
+    meaning: 'es gibt einen Nachteil, es wird nicht funktionieren.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Er erhebt sich wie Phönix aus der Asche"',
+    meaning:
+      'ein schon als verloren Abgeschriebener erscheint in neuem Glanze. Nach der griechischen Mythologie war der Vogel Phönix im Stande, zu verbrennen und aus seiner Asche wieder neu zu erstehen.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Von der Pike auf gelernt "',
+    meaning: 'sehr gründlich ausgebildet.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Wie Pilze aus dem Boden schießen "',
+    meaning:
+      'sich schlagartig vermehren, überhandnehmen. Vor allem negativ empfundene Dinge werden so charakterisiert    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"platt sein "',
+    meaning: 'erstaunt, positiv überrascht sein.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Viel Porzellan zerschlagen"',
+    meaning: 'sehr unangenehm auffallen. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Der springende Punkt "',
+    meaning: 'das Wesentliche, die Hauptsache.     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Quadratur des Kreises suchen"',
+    meaning: 'eine Aufgabe lösen wollen, die eigentlich unlösbar ist.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Qual der Wahl haben"',
+    meaning: 'eine schwere Entscheidung treffen müssen.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ein Quartalssäufer sein "',
+    meaning: 'nur selten, aber dann sehr viel Alkohol trinken.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"An der Quelle sitzen"',
+    meaning:
+      'problemlosen Zugang zu Dingen haben, die andere auch gerne hätten.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Eine gute Quelle haben "',
+    meaning: 'wissen, wo man etwas erhalten kann.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Etwas aus sicherer Quelle wissen"',
+    meaning: 'Kenntnis von einem guten Gewährsmann haben.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Mit jemandem quitt sein"',
+    meaning: 'mit jemandem alle ungeklärten Angelegenheiten bereinigt haben.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Quittung erhalten "',
+    meaning: 'die Folgen für ein bestimmtes Verhalten tragen müssen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Ein Rad ab haben"',
+    meaning: 'Unsinn reden, verrückt sein. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Im Rampenlicht stehen "',
+    meaning: 'öffentliche Aufmerksamkeit haben.    ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Mit etwas zu Rande kommen "',
+    meaning: 'mit etwas fertigwerden bzw. umgehen können. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Die Rechnung ohne den Wirt machen "',
+    meaning: 'sich täuschen, nicht die Folgen bedenken. ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Vom Regen in die Traufe kommen"',
+    meaning: 'eine noch schlimmere Situation gewärtigen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Jemanden im Regen stehen lassen"',
+    meaning: 'hn mit seinen Problemen und Sorgen allein lassen.',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Alle Register ziehen "',
+    meaning: 'alle Möglichkeiten nutzen/alle Hebel in Bewegung setzen.     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '"Von der Rolle sein"',
+    meaning:
+      ' unkonzentriert, verwirrt oder erschöpft sein, schlechtere Leistungen als gewöhnlich erbringen.     ',
+  },
+  {
+    _id: nanoid(),
+    expression: '""',
+    meaning: '',
+  },
+  {
+    _id: nanoid(),
+    expression: '"In China ist ein Sack Reis umgefallen"',
+    meaning: 'Ausdruck des eigenen Desinteresses.    ',
+  },
 ];
 
 // Quelle: https://de.m.wikipedia.org/wiki/Liste_deutscher_Redewendungen
